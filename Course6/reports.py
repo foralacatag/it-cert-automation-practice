d606ce62c03a5e68e7826db1621091591f355e26
Fatma Orsun <falacatag@bergenfield.org>
2020-07-29 00:21:57 -0400

    Update reports.py
    elements [] array is added to organize the content.

--- Course6/reports.py
+++ Course6/reports.py
@@ -15,11 +15,30 @@ def generate(filename, title, additional_info, table_data):
   report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
   empty_line = Spacer(1,20)
   report.build([report_title, empty_line, report_info, empty_line, report_table])
-  
+
 def generate_reports(filename, title, para):
+  elements = []
+  i=0
+
+  # Open the model report
+  file=open(para)
+  infile   = file.read()
+  report_paragraphs = infile.split("\n")
+  
   styles = getSampleStyleSheet()
   report = SimpleDocTemplate(filename)
   report_title = Paragraph(title, styles["h1"])
-  report_info = Paragraph(para, styles["BodyText"])
-  empty_line = Spacer(1,20)
-  report.build([report_title, empty_line, report_info])
+  elements.append(report_title)
+  empty_line = Spacer(1, 0)
+  elements.append(Spacer(1,2))
+  for param in report_paragraphs:
+    elements.append(Paragraph(param, styles["BodyText"]))
+    i=i+1
+    if i%2 ==0 :
+      elements.append(empty_line)
+      elements.append(empty_line)
+
+  print(elements)
+  #report_info = Paragraph(para, styles["BodyText"])
+
+  report.build(elements)
