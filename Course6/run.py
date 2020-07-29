5aa20c32e8b12bdec92213fa0f687e0fe1fb11b2
Fatma Orsun <falacatag@bergenfield.org>
2020-07-29 00:18:55 -0400

    Update run.py
    Runnig code is attached.

--- Course6/run.py
+++ Course6/run.py
@@ -6,7 +6,7 @@ import json
 import pathlib
 
 
-print(files)
+#print(files)
 
 def load_data(filename):
   """Loads the contents of filename as a JSON file."""
@@ -17,47 +17,51 @@ def load_data(filename):
 
 #list to hold each dictionary  from each file
 def data_dict():
-  home="/home/student-00-f3438403c3f4/supplier-data/descriptions/"
+  home="/home/student-02-a6b22d3abcd6/supplier-data/descriptions/"
   os.chdir(home)
   files = [f for f in os.listdir('.') if f.endswith('.txt')]
   sorted(files)
   list=[]
+  titles=["name", "weight", "description"]
 
-
-  titles=["name", "weight", "description", "image_name"]
-
-#iterate over each file to add it to the list
+  #iterate over each file to add it to the list
   for file in files:
+    command=0
+    print(file, command)
+    dict1={}
     with open(file) as fh:
-      command=0 
-      dict1={}
-      dict1[titles[3]]=os.path.basename(file[:-4])+".jpeg"
       for line in fh:
             # reads each line and trims of extra the spaces
             # and gives only the valid words
         description = line.strip()
         if command == 1:
-          dict1[titles[command]]=int(re.split('[\s,]+',description)[0])
+          dict1[titles[command]]=int(re.split('[\s,]+', description)[0])
         else:
           dict1[titles[command]] = description.strip()
         command+=1
-    #print(dict1)
+        if command>=3:
+          break
+    dict1["image_name"]=os.path.basename(file).split(".")[0]+".jpeg"
+
+    print(dict1)
     #Added creating text files with names and weights
-    with open("report.txt", ('a' if pathlib.Path('report.txt').exists() else 'w')) as file:
-      file.write(    "name: {}\nweight: {} lbs\n\n".format(dict1["name"], dict1["weight"])   )
+    with open("/home/student-02-a6b22d3abcd6/report.txt", 'a') as file:
+      file.write("name: {}\nweight: {} lbs\n".format(dict1["name"], dict1["weight"])   )
     list.append(dict1)
   return list
 
 #add list to json file
-with open('fruits.json', 'w') as fruit_json:
-    json.dumps(list, fruit_json, indent=2)
+#with open('fruits.json', 'w') as fruit_json:
+#    json.dump(list, fruit_json, indent=2)
 
 
 #Post each dictionary element to the website
-url = "http://localhost/fruits/"
+url = "http://35.232.202.143/fruits/"
 #with open("fruits.json", 'rb') as opened:
-elem=load_data(data_dict())
-print(elem)
-r = requests.post(url, data=elem)
-print(r.status_code)
 
+list=data_dict()
+#print(list)
+for elem in list:
+  d={"name":elem["name"], "weight":elem["weight"], "description":elem["description"], "image_name":elem["image_name"]}
+  r = requests.post(url, data=d)
+  print(r.status_code)
